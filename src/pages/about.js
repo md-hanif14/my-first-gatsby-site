e7f048f2165c9df7e04a926ee9251ac7c128e585
md-hanif14 <hanifuzzaman.programminghero@gmail.com>
2025-08-12 12:17:32 +0600

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage;
